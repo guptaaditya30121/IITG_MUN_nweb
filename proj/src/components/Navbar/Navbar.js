0532997f8212a6f203906cc47641374f41607f66
Ketan <ketansingh918@gmail.com>
2022-12-04 23:29:13 +0530

    Navbar fixed

--- proj/src/components/Navbar/Navbar.js
+++ proj/src/components/Navbar/Navbar.js
@@ -6,18 +6,11 @@ export default function Navbar() {
     <div className='navbar'>
       <img src={ require('../assets/logo.png')} alt="not_found" className='logo'></img>
       <ul className='list'>
-        <li className='list-item' href="www.google.com"><a className='link' href='www.google.com'>HOME</a></li>
-        <li className='list-item'>
-          <a className='link' href='www.google.com'>ABOUT US</a></li>
-        <li className='list-item'>
+        <a className='link' href='www.google.com'>HOME</a>
+        <a className='link' href='www.google.com'>ABOUT US</a>
         <a className='link' href='www.google.com'>APPLY</a>
-        </li>
-        <li className='list-item'>
         <a className='link' href='www.google.com'>APPLY</a>
-        </li>
-        <li className='list-item'>
         <a className='link' href='www.google.com'>MORE</a>
-        </li>
       </ul>
     </div>
   );
