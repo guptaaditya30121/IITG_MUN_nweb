4f87872b972d43a75b9c299add6bcd8a4c1552dc
aditya <guptaaditya30121@gmail.com>
2022-12-05 09:48:03 +0530

    changed

--- proj/src/components/Navbar/Navbar.js
+++ proj/src/components/Navbar/Navbar.js
@@ -1,19 +1,19 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 //import '../assets/logo.png';
 export default function Navbar() {
-  const [toggle , changetoggle] =useState(false);
-  const handletoggle = () =>changetoggle(!toggle);
+  const [toggle, changetoggle] = useState(false);
+  const handletoggle = () => changetoggle(!toggle);
   return (
     <div className='navbar'>
-      <img src={ require('../assets/logo.png')} alt="not_found" className='logo'></img>
-     
+      <img src={require('../assets/logo.png')} alt="not_found" className='logo'></img>
+
       <ul className='list'>
         <li><a className='link' href='www.google.com'>HOME</a></li>
         <li><a className='link' href='www.google.com'>ABOUT US</a></li>
         <li><a className='link' href='www.google.com'>APPLY</a></li>
         <li><a className='link' href='www.google.com'>MORE</a>
-            {/* <div className='sub-list'>
+          {/* <div className='sub-list'>
               <ul>
                 <li>fsd</li>
                 <li>fsd</li>
@@ -21,17 +21,27 @@ export default function Navbar() {
 
               </ul>
               </div>*/}
-              </li> 
-        
+        </li>
+
       </ul>
       <div className="menu-icon" onClick={handletoggle}>
-            <i className={toggle ? 'fas fa-times' : 'fas fa-bars'} />
+        {/* <i className={toggle ? 'fas fa-times' : 'fas fa-bars'} />
             <ul className={toggle ? 'active':'not-active'}>
             <li><a className='link2' href='www.google.com'>HOME    </a></li>
             <li><a className='link2' href='www.google.com'>ABOUT US </a></li>
             <li><a className='link2' href='www.google.com'>APPLY   </a></li>
             <li><a className='link2' href='www.google.com'>MORE</a></li>
-            </ul>
+            </ul> */}
+        <div className="btn-group dropstart">
+          <i className={toggle ? 'fas fa-times' : 'fas fa-bars'} data-bs-toggle="dropdown" aria-expanded="false"></i>
+          <ul className="dropdown-menu edit">
+            <li><a className="dropdown-item edit " href="#">HOME</a></li>
+            <li><a className="dropdown-item edit" href="#">ABOUT US</a></li>
+            <li><a className="dropdown-item edit" href="#">APPLY</a></li>
+            <li><a className="dropdown-item edit" href="#">MORE</a></li>
+
+          </ul>
+        </div>
       </div>
     </div>
   );
