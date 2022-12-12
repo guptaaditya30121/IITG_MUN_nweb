673d7141e2c4fc8658a821d6a086912056184707
Ketan <ketansingh918@gmail.com>
2022-12-12 17:13:46 +0530

    About us(only sign left)

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
--- proj/src/components/Footer/Footer.js
+++ proj/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 import { useState,useEffect } from "react";
 import "./Footer.css"
 import InstagramIcon from '@mui/icons-material/Instagram';
