2efc7dfc1f691edc51a9cb1a853f753e26a3b3e4
Sahil <g.sahil@iitg.ac.in>
2022-12-13 12:02:08 +0530

    alumni aos default_img

---
+++ proj/iitgmunnweb/schemas/alumni.js
@@ -0,0 +1,43 @@
+export default {
+    name: 'alumni',
+    type: 'document',
+    title: 'Alumni',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+        name: 'post',
+        type: 'string',
+        title:'Post'
+      },
+      {
+        name: 'img_url',
+        type: 'string',
+        title: 'Image URL'
+      },
+      {
+        name: 'status',
+        type: 'number',
+        title: 'Status'
+      },
+      {
+        name: 'id',
+        type: 'number',
+        title: 'ID'
+      },
+      {
+        name: 'alt',
+        type: 'string',
+        title: 'Alt'
+      },
+      {
+        name: 'tenure',
+        type: 'string',
+        title: 'Tenure'
+      },
+
+    ]
+}
